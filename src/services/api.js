c8f46cf23ed9666930644093089ba7e7c82cf424
Lucas Freitas Marques (CGIS/STIC/UNIC) <lucasfmarques@mec.gov.br>
2024-08-14 13:22:43 -0300

    Adicionando o .env

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: "163dfeb44132d15e16be542de4cbf259",
+        api_key: import.meta.env.VITE_API_KEY,
         language: 'pt-BR'
     }
 });
